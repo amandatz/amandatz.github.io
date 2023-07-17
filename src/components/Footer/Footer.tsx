c5be6b8dbb89baad11e3051ade35533dfbe8aef7
Amanda Zanette <amandatzanette@gmail.com>
2023-07-17 08:39:47 -0300

    - Adjusted footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,16 @@
 import styles from './Footer.module.scss';
 
+import { ButtonLink } from '@components';
+import { BiLogoLinkedin, BiLogoGithub } from 'react-icons/bi';
+
+const _iconSize = 28;
+
 const Footer = () => {
     return <footer className={styles.footer}>
-        Footer
+        <div className={styles.container}>
+            <ButtonLink href='/'><BiLogoGithub size={_iconSize} /></ButtonLink>
+            <ButtonLink href='/'><BiLogoLinkedin size={_iconSize} /></ButtonLink>
+        </div>
     </footer>;
 };
 
