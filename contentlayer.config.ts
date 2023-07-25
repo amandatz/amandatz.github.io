930aa3914702cfb847384a8c14c53a24ca842b44
Amanda Zanette <amandatzanette@gmail.com>
2023-07-25 12:10:22 -0300

    - Added math support

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,37 +1,43 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files"
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
-import rehypeAutolinkHeadings from "rehype-autolink-headings"
-import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
+import rehybeMathJax from 'rehype-mathjax'
+import remarkMath from 'remark-math'
 
 export const Post = defineDocumentType(() => ({
-    name: "Post",
+    name: 'Post',
     filePathPattern: `posts/*/content.mdx`,
-    contentType: "mdx",
+    contentType: 'mdx',
     fields: {
-        title: { type: "string", required: true },
-        description: { type: "string", required: true },
-        date: { type: "date", required: true },
-        tags: { type: "list", of: { type: "string", }, required: true },
+        title: { type: 'string', required: true },
+        description: { type: 'string', required: true },
+        date: { type: 'date', required: true },
+        tags: { type: 'list', of: { type: 'string', }, required: true },
     },
     computedFields: {
         slug: {
-            type: "string",
-            resolve: (doc) => doc._raw.flattenedPath.split("/")[1],
+            type: 'string',
+            resolve: (doc) => doc._raw.flattenedPath.split('/')[1],
         },
     },
 }))
 
 export default makeSource({
-    contentDirPath: "./src/data/",
+    contentDirPath: './src/data/',
     documentTypes: [Post],
     mdx: {
+        remarkPlugins:[
+            remarkMath
+        ],
         rehypePlugins: [
             rehypeSlug,
+            rehybeMathJax,
             [
                 rehypeAutolinkHeadings,
                 {
                     properties: {
-                        className: ["anchor"],
+                        className: ['anchor'],
                     },
                 },
             ],
--- src/lib/aboutme.ts
+++
@@ -1,9 +0,0 @@
-import { allAboutMes } from 'contentlayer/generated'
-
-export const getAboutMe = () => {
-    const aboutMe = allAboutMes.at(0)
-
-    if (!aboutMe) throw new Error(`No AboutMe found`)
-
-    return aboutMe
-}
