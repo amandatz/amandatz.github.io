f2ad6306bf4e0317fee2daa3cb13b958b9cd7bd1
Amanda Zanette <amandatzanette@gmail.com>
2023-07-31 15:25:57 -0300

    removed export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
-};
+}
 
 const { withContentlayer } = require('next-contentlayer')
 
